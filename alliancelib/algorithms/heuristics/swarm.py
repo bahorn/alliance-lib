25de18ffabeedc551a25d1f013edd67b8c220d56
B Horn <b@horn.uk>
2022-11-07 00:23:46 +0100

    Putting DA into a seperate mealpy problem
    Signed-off-by: B Horn <b@horn.uk>

--- alliancelib/algorithms/heuristics/swarm.py
+++ alliancelib/algorithms/heuristics/swarm.py
@@ -2,9 +2,11 @@
 Implementation of a Mealpy solver using ABC for Defensive Alliance
 """
 from alliancelib.ds.alliances.da import DefensiveAlliance
+from alliancelib.ds.vertex_set import ConstraintException
 from mealpy.swarm_based.ABC import OriginalABC
 from mealpy.swarm_based.BA import OriginalBA
 from mealpy.physics_based.SA import OriginalSA
+from mealpy.utils.problem import Problem
 from .cost_functions import da_score
 
 
@@ -64,6 +66,27 @@ def sa_model(pop_size=50, max_sub_iter=5, t0=1000, t1=1, move_count=5,
     return model
 
 
+class DefensiveAllianceProblem(Problem):
+    """
+    MealPy formulation of DefensiveAlliance
+    """
+
+    def __init__(self, graph, r=-1, name="DefensiveAlliance", **kwargs):
+        self.g = graph
+        self.r = r
+        self.vertex_count = len(self.g.nodes())
+        lb = [0 for i in range(self.vertex_count)]
+        ub = [1 for i in range(self.vertex_count)]
+        super().__init__(lb, ub, 'min', **kwargs)
+        self.name = name
+
+    def fit_func(self, solution):
+        new_solution = clean_solution(solution)
+        score = da_score(self.g, new_solution, r=self.r)
+        size = len(new_solution)/self.vertex_count
+        return (score, size)
+
+
 class DAMetaHeuristic:
     """
     Wrapper mealpy for solving Defensive Alliance
@@ -72,32 +95,24 @@ class DAMetaHeuristic:
     def __init__(self, model):
         self.model = model
 
-    def run(self, graph, generations=1000, r=-1):
+    def run(self, graph, generations=1000, r=-1, threads=8, time_limit=60):
         model = self.model(generations)
 
-        def fitness_function(solution):
-            new_solution = clean_solution(solution)
-            score = da_score(graph, new_solution, r=r)
-            size = len(new_solution)/len(graph.nodes())
-            return (score, size)
+        problem = DefensiveAllianceProblem(graph, obj_weights=[1.0, 0.1])
 
-        problem = {
-            "fit_func": fitness_function,
-            "lb": [0 for i in graph.nodes()],
-            "ub": [1 for i in graph.nodes()],
-            "minmax": "min",
-            "obj_weights": [1.0, 0.1]
+        term_dict = {
+            "mode": "TB",
+            "quantity": time_limit
         }
-
-        term_dict = {}
 
         best_position, best_fitness = model.solve(
             problem,
-            n_workers=8,
+            n_workers=threads,
             termination=term_dict
         )
 
-        da = DefensiveAlliance(graph, clean_solution(best_position), r=r)
-        print(f"Solution: {da}, Fitness: {best_fitness}")
-
-        return da
+        try:
+            da = DefensiveAlliance(graph, clean_solution(best_position), r=r)
+            print(f"Solution: {da}, Fitness: {best_fitness}")
+        except ConstraintException:
+            return None
--- examples/defensive_alliance_daabc.py
+++ examples/defensive_alliance_daabc.py
@@ -3,6 +3,6 @@ import networkx as nx
 from alliancelib.algorithms.heuristics.swarm import \
     abc_model, ba_model, sa_model, DAMetaHeuristic
 
-g = nx.gnp_random_graph(250, 0.05)
-solver = DAMetaHeuristic(sa_model())
-solver.run(g)
+g = nx.gnp_random_graph(1000, 0.25)
+solver = DAMetaHeuristic(abc_model())
+solver.run(g, time_limit=30)
